e10d6ed113215d0c0e26873f8af24dbad518f4f8
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2022-12-20 22:38:58 +0530

    mohanverse

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -4,7 +4,7 @@ const Counter = () => {
     const [Count, setCount] = useState(5)
   return (
     <>
-    <h1 style={{color: "#FFF"}}>Counter value - {Count}</h1>
+    <h1 style={{color: "#FFF"}}> Counter value - {Count}</h1>
     <button onClick={() => setCount(0)}>reset Counter</button>
     <button onClick={() => setCount( Count + 1 )}>increase Counter</button>
     <button onClick={() => setCount( Count - 1 )}>decrease Counter</button>
